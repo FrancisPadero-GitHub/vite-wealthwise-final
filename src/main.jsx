015ccd2431fcb5485e7615ec668da13a5a8706ac
Francis Padero <francispadero2001@gmail.com>
2025-05-16 23:04:20 +0800

    ambot 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import Signin from "./app/auth/SignInSide.jsx";
 import Signup from "./app/auth/SignUp.jsx";
-import Dashboard from "./app/components/dashboard/Dashboard.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Dashboard />
+    <Signin />
   </StrictMode>
 );
