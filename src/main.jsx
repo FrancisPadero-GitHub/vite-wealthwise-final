d2b75bd02c8f7be0c98863bedccd8826efef3881
Francis Padero <francispadero2001@gmail.com>
2025-05-16 22:34:59 +0800

    Ambot

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import Signin from "./app/auth/SignInSide.jsx";
+import Signup from "./app/auth/SignUp.jsx";
+import Dashboard from "./app/components/dashboard/Dashboard.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Dashboard />
+  </StrictMode>
+);
